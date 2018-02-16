f364ab8af060017855e66c207ec094250b2877b7
Siyuan Gao <sig019@ucsd.edu>
2018-02-15 23:25:54 -0800

    profile json modified

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,10 @@
  * GET settings page.
  */
 
+var data = require('../user.json');
+
 exports.view = function(req, res) {
-	res.render('profile'); 
+	console.log(data.rickord123[0].info[0]);
+	res.render('profile', data.rickord123[0].info[0]); 
 }
+
