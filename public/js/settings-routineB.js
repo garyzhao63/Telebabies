c0fd4634a29fa00496b0654e31819b71fa78995b
Siyuan Gao <sig019@ucsd.edu>
2018-03-01 19:18:32 -0800

    add alert page version B added

---
+++ public/js/settings-routineB.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var data = JSON.parse(window.localStorage.getItem("user"));
+var list = JSON.parse($.ajax({type: "GET", url: "rList", async: false}).responseText);
+
+var selectedTime;
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#demoRepeat").html("daily");
+	mobiscroll.time('#demo', {
+		onSet: function (event, inst) {
+            selectedTime = inst.getVal(); // Call the getVal method
+       		console.log(selectedTime);
+    	},
+        theme: 'ios-dark',     // Specify theme like: theme: 'ios' or omit setting to use default
+        lang: 'en',            // Specify language like: lang: 'pl' or omit setting to use default
+        display: 'bottom'      // Specify display mode like: display: 'bottom' or omit setting to use default
+    }); 
+
+	console.log("Javascript connected!");
+	$("#add-button").click(confirm);
+	$("#demoRepeat").click(repeatClick);
+}
+
+function confirm(e) {
+	e.preventDefault();
+	// var selectedTime = $("#demo").mobiscroll('getVal');
+	// console.log(selectedTime);
+	var dateObj = new Date(Date.parse(selectedTime)); 
+	console.log(dateObj.getHours());
+	console.log(dateObj.getMinutes());
+
+	var size = data.routine.length;
+	if (dateObj.getHours() && dateObj.getMinutes()) {
+
+		var minuteStr = dateObj.getMinutes().toString();
+		if (dateObj.getMinutes().toString().length < 2) {
+			minuteStr = "0" + dateObj.getMinutes();
+		}
+
+		var hourStr = dateObj.getHours().toString();
+		var M; 
+		if (dateObj.getHours().toString().length < 2) {
+			hourStr = "0" + dateObj.getHours();
+		}
+		if (dateObj.getHours() > 12) {
+			hourStr = (dateObj.getHours() - 12).toString(); 
+			M = ' PM'; 
+		} else if (dateObj.getHours() == 12) {
+			M = ' PM'; 
+		} else {
+			M = ' AM'; 
+		}
+
+		var newJSON = {
+			"time": hourStr + ":" + minuteStr + M, 
+			"repeat": $("#demoRepeat").html(),
+			"id": size
+		};
+
+		data.routine.push(newJSON);
+		list[data.username] = data;
+		window.localStorage.setItem("user", JSON.stringify(data));
+		$.post('wList', list); 
+		location.href = '/settings'; 
+	} else {
+		alert("Please choose a time."); 
+	}
+	
+
+
+}
+
+
+function repeatClick(e) {
+
+	e.preventDefault();
+	var curRepeat = $(this).html();
+
+	if(curRepeat == "daily") {
+		$(this).html("weekly");
+	} 
+	else if (curRepeat == "weekly") {
+		$(this).html("monthly");
+	} 
+	else {
+		$(this).html("daily");
+	}
+
+}
+
